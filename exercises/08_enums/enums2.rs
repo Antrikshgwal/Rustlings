1c5f77c662aa2a55158fc5aaeef003148035793f
Antriksh Gwal <antrikshgwal@gmail.com>
2026-01-15 23:26:11 +0530

    Hashmap completed

---
+++ exercises/08_enums/enums2.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+    Resize(),
+    Move(u8,u8),
+    Echo(String),
+    ChangeColor(u8,u8,u8),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{self:?}");
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Resize(),
+        Message::Move(10,15),
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
