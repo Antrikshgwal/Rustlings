5dd81ce225a9aa921dcdf2012a7e6b1c33b7952f
Antriksh Gwal <antrikshgwal@gmail.com>
2026-01-15 23:42:58 +0530

    add projects

---
+++ projects/minigrep/src/main.rs
@@ -0,0 +1,50 @@
+use std::{env, error::Error, fs, process};
+use minigrep::{search, search_case_insensitive};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error:  {e}");
+        process::exit(1);
+    }
+
+}
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignored_case {
+        search_case_insensitive(&config.query, &contents )
+    }else {
+        search(&config.query, &contents)
+    };
+for line in results{
+    println!("{line}")
+}
+
+    Ok(())
+}
+
+struct Config {
+    // Using String instead of a reference to a string could be an issue
+    query: String,
+    file_path: String,
+    ignored_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignored_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config { query, file_path, ignored_case })
+    }
+}
